f9507b0f8d0db9f8cdd6ba91ebbffe9643984f1a
Rallapati <Rallapati@Lenovo-PC>
2019-10-19 01:25:32 +0530

    second coomit

---
+++ githubprj/src/githubprj/Demo1.java
@@ -0,0 +1,10 @@
+package githubprj;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 System.out.println("pranitha pranitha pranitha");
+	}
+
+}
